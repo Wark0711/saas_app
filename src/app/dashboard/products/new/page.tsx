fae3f305c60142a59d1429872666bd46b43d70a7
Wark0711 <mickey.rahul98@gmail.com>
2024-11-10 00:20:09 +0530

    Added products page layout and completed products section

--- src/app/dashboard/products/new/page.tsx
+++ src/app/dashboard/products/new/page.tsx
@@ -1,10 +1,17 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { PageWithBackButton } from "../../_components/PageWithBackButton";
 import { ProductDetailsForm } from "../../_components/forms/ProductDetailsForm";
+import { canCreateProduct } from "@/server/permissions";
+import { HasPermission } from "@/components/HasPermission";
 
 export default function NewProductPage() {
     return (
         <PageWithBackButton backButtonHref={'/dashboard/products'} pageTitle="Create Product">
+            <HasPermission
+                permission={canCreateProduct}
+                renderFallback
+                fallbackText={'You have already created the maximum number of products. Try upgrading your account to create more.'}
+            >
             <Card>
                 <CardHeader>
                     <CardTitle className="text-xl">Product Details</CardTitle>
@@ -13,6 +20,7 @@ export default function NewProductPage() {
                     <ProductDetailsForm />
                 </CardContent>
             </Card>
+            </HasPermission>
         </PageWithBackButton>
     )
 }
