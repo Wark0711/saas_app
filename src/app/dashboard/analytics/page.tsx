13eaba5ca915888ecff77dac8f645979abecba7a
Wark0711 <mickey.rahul98@gmail.com>
2024-11-12 23:40:13 +0530

    Added 2 charts in analytics dashboard

---
+++ src/app/dashboard/analytics/page.tsx
@@ -0,0 +1,81 @@
+import { HasPermission } from "@/components/HasPermission"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { CHART_INTERVALS, getViewsByCountryChartData, getViewsByPPPChartData } from "@/server/db/productViews"
+import { canAccessAnalytics } from "@/server/permissions"
+import { auth } from "@clerk/nextjs/server"
+import { ViewsByCountryChart } from "../_components/charts/ViewsByCountryChart"
+import { ViewsByPPPChart } from "../_components/charts/ViewsByPPPChart"
+
+export default async function AnalyticsPage({ searchParams }: {
+    searchParams: {
+        interval?: string,
+        timeZone?: string,
+        productId?: string
+    }
+}) {
+
+    const { userId, redirectToSignIn } = await auth()
+    if (userId == null) return redirectToSignIn()
+
+    const timeZone = searchParams.timeZone || 'UTC'
+    const productId = searchParams.productId
+    const interval = CHART_INTERVALS[searchParams.interval as keyof typeof CHART_INTERVALS] ?? CHART_INTERVALS.last7Days
+
+    return (
+        <>
+            <h1 className="text-3xl font-semibold">Analytics</h1>
+            <HasPermission permission={canAccessAnalytics} renderFallback>
+                <div className="flex flex-col gap-8">
+                    {/* <ViewsByDayCard interval={interval} timeZone={timeZone} userId={userId} productId={productId} /> */}
+                    <ViewsByPPPCard interval={interval} timeZone={timeZone} userId={userId} productId={productId} />
+                    <ViewsByCountryCard interval={interval} timeZone={timeZone} userId={userId} productId={productId} />
+                </div>
+            </HasPermission>
+        </>
+    )
+}
+
+// async function ViewsByDayCard(props: Parameters<typeof getViewsByDayChartData>[0]) {
+//     const chartData = await getViewsByDayChartData(props)
+
+//     return (
+//         <Card>
+//             <CardHeader>
+//                 <CardTitle>Visitors Per Day</CardTitle>
+//             </CardHeader>
+//             <CardContent>
+//                 <ViewsByDayChart chartData={chartData} />
+//             </CardContent>
+//         </Card>
+//     )
+// }
+
+async function ViewsByPPPCard(props: Parameters<typeof getViewsByPPPChartData>[0]) {
+    const chartData = await getViewsByPPPChartData(props)
+
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Visitors Per PPP Group</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <ViewsByPPPChart chartData={chartData} />
+            </CardContent>
+        </Card>
+    )
+}
+
+async function ViewsByCountryCard(props: Parameters<typeof getViewsByCountryChartData>[0]) {
+    const chartData = await getViewsByCountryChartData(props)
+
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Visitors Per Country</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <ViewsByCountryChart chartData={chartData} />
+            </CardContent>
+        </Card>
+    )
+}
--- src/server/db/productViews.ts
+++ src/server/db/productViews.ts
@@ -1,7 +1,9 @@
 import { db } from "@/drizzle/db";
-import { ProductTable, ProductViewTable } from "@/drizzle/schema";
-import { CACHE_TAGS, dbCache, getUserTag, revalidateDbCache } from "@/lib/cache";
-import { and, count, eq, gte } from "drizzle-orm";
+import { CountryGroupTable, CountryTable, ProductTable, ProductViewTable } from "@/drizzle/schema";
+import { CACHE_TAGS, dbCache, getGlobalTag, getIdTag, getUserTag, revalidateDbCache } from "@/lib/cache";
+import { startOfDay, subDays } from "date-fns";
+import { and, count, desc, eq, gte, sql } from "drizzle-orm";
+import { tz } from '@date-fns/tz'
 
 export function getProductViewCount(userId: string, startDate: Date) {
     const cacheFn = dbCache(getProductViewCountInternal, {
@@ -27,7 +29,42 @@ export async function createProductView({ productId, countryId, userId }: {
     }
 }
 
-export async function getProductViewCountInternal(userId: string, startDate: Date) {
+export async function getViewsByCountryChartData({ timeZone, productId, userId, interval }: {
+    timeZone: string,
+    productId?: string,
+    userId: string,
+    interval: (typeof CHART_INTERVALS)[keyof typeof CHART_INTERVALS]
+}) {
+    const cacheFn = dbCache(getViewsByCountryChartDataInternal, {
+        tags: [
+            getUserTag(userId, CACHE_TAGS.productViews),
+            productId == null ? getUserTag(userId, CACHE_TAGS.products) : getIdTag(productId, CACHE_TAGS.products),
+            getGlobalTag(CACHE_TAGS.countries)
+        ]
+    })
+
+    return cacheFn({ timeZone, productId, userId, interval })
+}
+
+export async function getViewsByPPPChartData({ timeZone, productId, userId, interval }: {
+    timeZone: string,
+    productId?: string,
+    userId: string,
+    interval: (typeof CHART_INTERVALS)[keyof typeof CHART_INTERVALS]
+}) {
+    const cacheFn = dbCache(getViewsByPPPChartDataInternal, {
+        tags: [
+            getUserTag(userId, CACHE_TAGS.productViews),
+            productId == null ? getUserTag(userId, CACHE_TAGS.products) : getIdTag(productId, CACHE_TAGS.products),
+            getGlobalTag(CACHE_TAGS.countries),
+            getGlobalTag(CACHE_TAGS.countryGroups)
+        ]
+    })
+
+    return cacheFn({ timeZone, productId, userId, interval })
+}
+
+async function getProductViewCountInternal(userId: string, startDate: Date) {
     const counts = await db
         .select({ pricingViewCount: count() })
         .from(ProductViewTable)
@@ -36,3 +73,95 @@ export async function getProductViewCountInternal(userId: string, startDate: Dat
 
     return counts?.at(0)?.pricingViewCount ?? 0
 }
+
+async function getViewsByCountryChartDataInternal({ timeZone, productId, userId, interval }: {
+    timeZone: string,
+    productId?: string,
+    userId: string,
+    interval: (typeof CHART_INTERVALS)[keyof typeof CHART_INTERVALS]
+}) {
+    const startDate = startOfDay(interval.startDate, { in: tz(timeZone) })
+    const productsSq = getProductSubQuery(userId, productId)
+
+    return await db
+        .with(productsSq)
+        .select({
+            views: count(ProductViewTable.visitedAt),
+            countryName: CountryTable.name,
+            countryCode: CountryTable.code,
+        })
+        .from(ProductViewTable)
+        .innerJoin(productsSq, eq(productsSq.id, ProductViewTable.productId))
+        .innerJoin(CountryTable, eq(CountryTable.id, ProductViewTable.countryId))
+        .where(
+            gte(
+                sql`${ProductViewTable.visitedAt} AT TIME ZONE ${timeZone}`.inlineParams(),
+                startDate
+            )
+        )
+        .groupBy(({ countryCode, countryName }) => [countryCode, countryName])
+        .orderBy(({ views }) => desc(views))
+        .limit(25)
+}
+
+async function getViewsByPPPChartDataInternal({ timeZone, productId, userId, interval }: {
+    timeZone: string,
+    productId?: string,
+    userId: string,
+    interval: (typeof CHART_INTERVALS)[keyof typeof CHART_INTERVALS]
+}) {
+
+    const startDate = startOfDay(interval.startDate, { in: tz(timeZone) })
+    const productsSq = getProductSubQuery(userId, productId)
+    const productViewSq = db.$with("productViews").as(
+        db
+            .with(productsSq)
+            .select({
+                visitedAt: sql`${ProductViewTable.visitedAt} AT TIME ZONE ${timeZone}`
+                    .inlineParams()
+                    .as("visitedAt"),
+                countryGroupId: CountryTable.countryGroupId,
+            })
+            .from(ProductViewTable)
+            .innerJoin(productsSq, eq(productsSq.id, ProductViewTable.productId))
+            .innerJoin(CountryTable, eq(CountryTable.id, ProductViewTable.countryId))
+            .where(({ visitedAt }) => gte(visitedAt, startDate))
+    )
+
+    return await db
+        .with(productViewSq)
+        .select({
+            pppName: CountryGroupTable.name,
+            views: count(productViewSq.visitedAt),
+        })
+        .from(CountryGroupTable)
+        .leftJoin(
+            productViewSq,
+            eq(productViewSq.countryGroupId, CountryGroupTable.id)
+        )
+        .groupBy(({ pppName }) => [pppName])
+        .orderBy(({ pppName }) => pppName)
+
+}
+
+function getProductSubQuery(userId: string, productId?: string) {
+    return db.$with('products').as(db.select().from(ProductTable).where(and(eq(ProductTable.clerkUserId, userId), productId == null ? undefined : eq(ProductTable.id, productId))))
+}
+
+export const CHART_INTERVALS = {
+    last7Days: {
+        startDate: subDays(new Date(), 7),
+        label: "Last 7 Days"
+    },
+    last30Days: {
+        startDate: subDays(new Date(), 30),
+        label: "Last 30 Days"
+    },
+    last365Days: {
+        startDate: subDays(new Date(), 365),
+        label: "Last 365 Days"
+    }
+}
+
+
+
